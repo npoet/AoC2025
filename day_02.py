44bbc00ced42b42ba90cb23bb2dac2f68db7b27a
npoet <nicholasjpoet@gmail.com>
2025-12-07 00:14:47 -0800

    add day 2 part 1 solution

--- day_02.py
+++ day_02.py
@@ -1,7 +1,20 @@
 def part_one():
     with open("inputs/day_02.txt") as f:
         ranges = f.readline().strip().split(",")
-    return ranges
+        invalid_ids = 0
+        for id_range in ranges:
+            id_min, id_max = id_range.split("-")
+            for num in range(int(id_min), int(id_max) + 1):
+                n = len(str(num))
+                if n % 2 != 0:
+                    continue
+                half = n // 2
+                first = str(num)[:half]
+                second = str(num)[half:]
+                if first == second:
+                    invalid_ids += num
+    return invalid_ids
+
 
 if __name__ == "__main__":
     print(part_one())
